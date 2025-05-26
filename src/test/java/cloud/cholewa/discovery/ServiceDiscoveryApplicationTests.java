05b0d2cc9b8bba0575338b462094b227f3c6c97e
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2025-05-26 18:38:49 +0200

    initial commit

---
+++ src/test/java/cloud/cholewa/discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
